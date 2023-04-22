947b32ca87c6c60cc4a0b96f90a8b9eb7e586800
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-04-22 12:52:46 +0530

    added config.yaml file

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -4,7 +4,8 @@ from collections import namedtuple
 DataIngestionConfig = namedtuple("DataIngestionConfig", 
                                  ["raw_data_dir", "ingested_train_dir", "ingested_test_dir"])
 
-DataValidationConfig = namedtuple("DataValidationConfig",  ["schema_file_path"])
+DataValidationConfig = namedtuple("DataValidationConfig", 
+                                  ["schema_file_name"])
 
 DataTransformationConfig = namedtuple("DataTransformationConfig", 
                                       ['transformed_train_dir', 'transformed_test_dir', 'preprocessed_object_file_path'])
