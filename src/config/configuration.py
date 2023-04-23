f01be05dd808105c10c77e6e5a8cb0b6449a4a49
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2023-04-23 12:34:43 +0530

    added validation and transformation config

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -82,20 +82,104 @@ class Configuration:
 
 
     def get_data_validation_config(self) -> DataValidationConfig:
-        pass
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+
+            data_validation_artifact_dir = os.path.join(
+                artifact_dir,
+                DATA_VALIDATION_ARTIFACT_DIR_NAME,
+                self.time_stamp
+            )
+
+            data_validation_info = self.config_info[DATA_VALIDATION_CONFIG_KEY]
+
+            schema_file_path = os.path.join(
+                ROOT_DIR,
+                data_validation_info[DATA_VALIDATION_SCHEMA_DIR_KEY],
+                data_validation_info[DATA_VALIDATION_SCHEMA_FILE_NAME_KEY]
+            )
+
+
+            report_file_path = os.path.join(
+                data_validation_artifact_dir,
+                data_validation_info[DATA_VALIDATION_REPORT_FILE_NAME_KEY]
+            )
+
+
+            report_page_file_path = os.path.join(
+                data_validation_artifact_dir,
+                data_validation_info[DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY]
+            )
+
+            data_validation_config = DataValidationConfig(schema_file_path= schema_file_path, report_file_path= report_file_path, report_page_file_path= report_page_file_path)
+            
+            logging.info(f"Data Validation config: {data_validation_config}")
+            return data_validation_config
+
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
     def get_data_transformation_config(self) -> DataTransformationConfig:
-        pass
+        try:
+            artifact_dir = self.training_pipeline_config.artifact_dir
+
+            data_transformation_artifact_dir = os.path.join(
+                artifact_dir,
+                DATA_TRANSFORMATION_DIR_NAME_KEY,
+                self.time_stamp 
+            )
+
+
+            data_transformation_info = self.config_info[DATA_TRANSFORMATION_CONFIG_KEY]
+
+            preprocessed_object_file_path = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_info[DATA_TRANSFORMATION_PREPROCESSED_DIR_KEY],
+                data_transformation_info[DATA_TRANSFORMATION_PREPROCESSED_FILE_NAME_KEY]
+            )
+
+            transformed_train_dir = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_info[DATA_TRANSFORMATION_DIR_NAME_KEY],
+                data_transformation_info[DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY]
+            )
+
+            transformed_test_dir = os.path.join(
+                data_transformation_artifact_dir,
+                data_transformation_info[DATA_TRANSFORMATION_DIR_NAME_KEY],
+                data_transformation_info[DATA_TRANSFORMATION_TEST_DIR_NAME_KEY]
+            )
+
+
+            data_transformation_config = DataTransformationConfig(
+                transformed_train_dir= transformed_train_dir, 
+                transformed_test_dir= transformed_test_dir, 
+                preprocessed_object_file_path= preprocessed_object_file_path
+            )
+
+            logging.info(f"Data Transformation config: {data_transformation_config}")
+            return data_transformation_config
+
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
     def get_model_trainer_config(self) -> ModelTrainerConfig:
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
 
     def get_model_evaluation_config(self) -> ModelEvaluationConfig:
-        pass
+        try:
+            pass
+
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
     def get_model_pusher_config(self) -> ModelPusherConfig: 
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -27,3 +27,22 @@ DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
 DATA_INGESTION_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
+
+# Data validation related constants
+DATA_VALIDATION_ARTIFACT_DIR_NAME = "data_validation"
+DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
+DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = "report_file_name"
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = "report_page_file_name"
+
+
+# Data transformation related constants
+DATA_TRANSFORMATION_ARTIFACT_DIR_NAME = "data_transformation"
+DATA_TRANSFORMATION_CONFIG_KEY = "data_transformation_config"
+DATA_TRANSFORMATION_DIR_NAME_KEY = "transformed_dir"
+DATA_TRANSFORMATION_TRAIN_DIR_NAME_KEY = "transformed_train_dir"
+DATA_TRANSFORMATION_TEST_DIR_NAME_KEY = "transformed_test_dir"
+DATA_TRANSFORMATION_PREPROCESSED_DIR_KEY = "preprocessing_dir"
+DATA_TRANSFORMATION_PREPROCESSED_FILE_NAME_KEY = "preprocessed_object_file_name"
